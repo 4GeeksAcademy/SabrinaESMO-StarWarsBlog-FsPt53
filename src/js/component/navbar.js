f21f26e60253c3b6c2be619b617a1d8812456a37
Sabrina Estévez <sesmo.dev@gmail.com>
2024-02-07 17:29:34 +0000

    avance NavBar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,28 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar bg-dark mb-3 p-3">
+			<div className="container-fluid">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1 text-warning">Star Wars Blog by Sesmodev</span>
 				</Link>
+				
+				<div>
+					<button className="navbar-toggler" data-bs-toggle="collapse">
+						Favorites
+						<span className="navbar-toggler-icon"></span>
+						<ul>
+							<li><Link>Fav 1</Link></li>
+							<li><Link>Fav 2</Link></li>
+							<li><Link>Fav 3</Link></li>
+						</ul>
+					</button>
+				</div>
+
+				<form className="d-flex" role="search">
+					<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+					<button className="btn btn-outline-success" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+				</form>
 			</div>
 		</nav>
 	);
