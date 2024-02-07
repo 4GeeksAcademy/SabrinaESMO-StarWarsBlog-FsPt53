1619dc3aa79079604855669da794eef58bf56d18
Sabrina Estévez <sesmo.dev@gmail.com>
2024-02-07 16:49:19 +0000

    footer listo, pendiente revisión final

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
+	<footer className="footer sticky-bottom text-center bg-dark">
+		<p className="text-warning mt-5 pt-2 pb-2">
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="https://github.com/SabrinaESMO">SabrinaESMO</a>
 		</p>
 	</footer>
 );
