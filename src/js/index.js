8a1b04bda7f00b2dcb400fdeba77be0b721fb1db
Sabrina Estévez <sesmo.dev@gmail.com>
2024-02-10 19:54:01 +0000

    primeros pasos character card

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Layout from './layout.js'
+import { CharacterCard } from './component/CharacterCard.js';
 
 //
 const root = createRoot(document.querySelector("#app"))
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { CharacterCard } from "../component/CharacterCard";
 
 export const Home = () => (
 	<div className="text-center mt-5">
@@ -11,5 +12,7 @@ export const Home = () => (
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
+
+
 	</div>
 );
